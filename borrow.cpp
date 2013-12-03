39c234f522e43ed81a79220f081d071e5754de31
Rodelle Ladia Jr <rLadia@gmail.com>
2013-12-03 01:59:20 -0800

    Set name property; correctly adds self to customers list of transactions

--- borrow.cpp
+++ borrow.cpp
@@ -3,8 +3,10 @@
 #include "inventoryitem.h"
 
 Borrow::Borrow(StoreCustomer& customer, InventoryItem& item)
-  : Transaction(customer, item) 
-{}
+  : Transaction(customer, item)
+{
+  name_ = "BORROW";
+}
 
 Borrow::~Borrow()
 {}
@@ -12,13 +14,14 @@ Borrow::~Borrow()
 bool Borrow::ExecuteTransaction()
 {
   if(isTransactionComplete_) // transactions can only occur once
-    return false; 
-  
+    return false;
+
   if(item_.GetInventoryCount() <= 0) // not enough movies exist
     return false;
 
   item_.RemoveFromInventory(1);
-  customer_.CheckoutMovie(&item_.movie()); 
+  customer_.CheckoutMovie(&item_.movie());
+  customer_.AddTransaction(this);
   isTransactionComplete_ = true;
 
   return true;
