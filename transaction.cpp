c6db3fc5a1894d25a4935eef76ba8aceeed337fe
Rodelle Ladia Jr <rLadia@gmail.com>
2013-12-03 02:01:05 -0800

    Added name property and method of printing out transaction information

--- transaction.cpp
+++ transaction.cpp
@@ -3,8 +3,19 @@
 #include "transaction.h"
 
 Transaction::Transaction(StoreCustomer& customer, InventoryItem& item)
-  : customer_(customer), item_(item), isTransactionComplete_(false)
+  : customer_(customer), item_(item), isTransactionComplete_(false), name_("")
 {}
 
 Transaction::~Transaction()
 {}
+
+std::ostream& operator<<(std::ostream& out, const Transaction& transaction)
+{
+  out << InventoryItem::kDefaultMediaName
+    << " " << transaction.name_
+    << "  " << transaction.item_.movie();
+  return out;
+}
+
+const std::string& Transaction::name() const
+{ return name_; }
--- transaction.h
+++ transaction.h
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Transaction.h
-// Transaction is a base class from which the different concrete types of 
+// Transaction is a base class from which the different concrete types of
 // derived transactions inherit. Each transaction represents an association
 // between a customer and an inventoryitem
 ///////////////////////////////////////////////////////////////////////////////
@@ -8,10 +8,13 @@
 #ifndef CSS343_LAB4_TRANSACTION_H
 #define CSS343_LAB4_TRANSACTION_H
 
+#include <string>
+
 class StoreCustomer;
 class InventoryItem;
 
 class Transaction {
+friend std::ostream& operator<<(std::ostream&, const Transaction&);
 public:
   // Transactions can only exist between a Customer and a MovieItem
   Transaction(StoreCustomer&, InventoryItem&);
@@ -20,12 +23,15 @@ public:
   // Performs the necessary operations to execute the transaction
   virtual bool ExecuteTransaction() = 0;
 
+  virtual const std::string& name() const;
+
 protected:
   StoreCustomer& customer_;
   InventoryItem& item_;
 
   // Transactions can only be completed once
   bool isTransactionComplete_;
+  std::string name_;
 };
 
 #endif
